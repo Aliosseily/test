b3ba6aa2093112cddadb160b47b4e7f738e0f3d8
Aliosseily <ali.ossaily@outlook.com>
2018-05-13 14:51:48 +0300

    test

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ if(guess < randomNumber){
     }
     else {
 counter++;
-   result="small";rthrthrt5uhw45y
+   result="small";
     }
 }
 
@@ -46,6 +46,9 @@ d.innerHTML="number of trials : "+ counter;
 }
 c.addEventListener('click', guessGame);
 
+<<<<<<< HEAD
 //Ali ossaily first
 
 //Ali ossaily second
+=======
+>>>>>>> parent of 8083032... second
